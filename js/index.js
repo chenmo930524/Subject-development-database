3b7c41765bf4c8f5bee340cdbec1061e371869b6
chenmo930524 <15151960159@139.com>
2017-04-14 16:50:36 +0800

    subjectDevelopmentDatab
    Version2 Optimized BaiDu maps

--- js/index.js
+++ js/index.js
@@ -8,8 +8,6 @@ $(function () {
 		}
 	})
 
-
-
 	//scroll
 	$(".ul-top li").eq(0).click(function () {
 		$("body").stop(true).animate({
@@ -85,17 +83,15 @@ $(function () {
 	$(window).scroll(function () {
 		var i = $(this).scrollTop();
 		var h1 = $(".body1").height();
-		var h1 = $(".body1").height();
 		var h2 = $(".body2").height();
 		var h3 = $(".body3").height();
 		var h4 = $(".body4").height();
-		if (i < h1) {
-			$(".body5b-test").hide();
-		} else if (i >= h1 / 3 && i < h1 + 50) {
+		if (i >= h1 / 3 && i < h1 + 50) {
 			$(".body2-l .p1").fadeIn(1500);
 			$(".body2-l .p2").fadeIn(1500);
 			$(".body2-r span").fadeIn(2000);
 			$(".body2-r .p1").fadeIn(3000);
+			$(".body5b-test").hide();
 		} else if (i >= h1 + 50 && i < h1 + h2 + 50) {
 			$(".body5b-test").show();
 			$(".body3-1").fadeIn(1500);
@@ -111,41 +107,52 @@ $(function () {
 			$(".info2").fadeIn(4000);
 			$(".info3").fadeIn(5000);
 		};
-			
-	
-		
+		if ($(".menu").hasClass("h")) {} else {
+			$(".menu").addClass("h");
+		}
 	})
 
 
 	//baidumap
-	var map = new BMap.Map("allmap");
-	var point = new BMap.Point(118.786511, 32.029147);
-	map.centerAndZoom(point, 15);
-	var marker = new BMap.Marker(point); // 创建标注
-	map.addOverlay(marker); // 将标注添加到地图中
-	marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
-	var opts = {
-		position: point, // 指定文本标注所在的地理位置
-		offset: new BMap.Size(10, -70) //设置文本偏移量
-	}
-	var label = new BMap.Label("欣才IT学院", opts); // 创建文本标注对象
-	label.setStyle({
-		color: "red",
-		fontSize: "14px",
-		height: "20px",
-		lineHeight: "20px",
-		borderRadius: "5px",
-		fontFamily: "微软雅黑"
-	});
-	map.addOverlay(label);
-	map.addEventListener("tilesloaded", function () {
-		map.setCenter(point);
-	});
 	$(".btn").on("click", function () {
-		$(".MAP").show();
+		$(".MAP").show();		
+		//MAP初始化
+		var map = new BMap.Map("allmap");
+		var point = new BMap.Point(118.786511, 32.029147);
+		map.centerAndZoom(point, 15);
+		var marker = new BMap.Marker(point); // 创建标注
+		map.addOverlay(marker); // 将标注添加到地图中
+		marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+		map.enableScrollWheelZoom(); //启动鼠标滚轮缩放地图		
+		//文本
+		var opts = {
+			position: point, // 指定文本标注所在的地理位置
+			offset: new BMap.Size(10, -70) //设置文本偏移量
+		}
+		var label = new BMap.Label("欣才IT学院", opts); // 创建文本标注对象
+		label.setStyle({
+			color: "red",
+			fontSize: "14px",
+			height: "20px",
+			lineHeight: "20px",
+			borderRadius: "5px",
+			fontFamily: "微软雅黑"
+		});
+		map.addOverlay(label);
+		//缩放回中心
+		$(window).on("resize", function () {
+			var loadCount = 1;
+			map.addEventListener("tilesloaded", function () {
+				if (loadCount == 1) {
+					map.setCenter(point);
+				}
+				loadCount = loadCount + 1;
+			});
+		})
 	})
+	
+	
 	$("#close").on("click", function () {
 		$(".MAP").hide();
 	})
-
 })
